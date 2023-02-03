858aa1d37ee606d502ef5ef108bfed9a3daa3836
stella-te <stella@tradingeconomics.com>
2023-02-03 13:50:19 +0200

    token

--- git_cmd.py
+++ git_cmd.py
@@ -2,9 +2,6 @@ git init
 
 curl -u 'stella-te' https://api.github.com/user/repos -d '{"name":"mssql"}'
 
-# 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
-# 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
-
 git remote add origin https://github.com/stella-te/mssql.git
 
 echo "#Don't panic, it's not rocket science." -> README.md
@@ -17,6 +14,10 @@ git remote set-url origin https://github.com/stella-vir/REPO.git
 
 git push --set-upstream origin main
 
+git rm -r --cached .
+
+git checkout main
+
 del credential.helper=osxkeychain
 
 git config --list
@@ -25,9 +26,7 @@ git config --global user.name "stella-te"
 
 git config --global user.email "stella@tradingeconomics.com"
 
-
-
----------------------------------------------
 .git/config
 
-git rm -r --cached .
+
+# end
