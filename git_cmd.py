11a804bec01e438e85dd0249e1c214c74343052c
stella-vir <stellavir11@gmail.com>
2023-02-03 13:02:49 +0200

    first commit

---
+++ git_cmd.py
@@ -0,0 +1,13 @@
+git init
+
+curl -u 'stella-te' https://api.github.com/user/repos -d '{"name":"mssql"}'
+
+# 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
+
+git remote add origin https://github.com/stella-te/mssql.git
+
+echo "#Don't panic, it's not rocket science." -> README.md
+
+git add .
+
+git commit -m "first commit"
