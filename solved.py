b1a76d04f55ee98d26c32f36ca5311daee2e033e
stella <stella@tradingeconomics.com>
2023-02-08 17:53:55 +0200

    Add files via upload

---
+++ solved.py
@@ -0,0 +1,44 @@
+6th
+
+Sql
+Vs solution data connection design grid view
+1 IIS
+
+vs run as admin
+host file
+Port number
+Web deployment enable iis ui, handler and remote agent service
+Turn win feature on and off, turn off iis www
+2 security authentication
+
+
+
+7th
+
+server with data deploy on vs publish and iis
+server script for another computer
+
+
+javascript name and price arr.push(obj)  txt
+
+
+Insert into values
+ select * from Json file bulk cross
+
+server data connection db conn
+
+
+
+8th
+
+js convert js two json files concat  symbol to name
+
+cast sql data type
+
+
+
+3 kafka
+
+
+
+ssh -i university.pem ubuntu@18.206.150.247
